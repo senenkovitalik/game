cc4c2d0ff6de984fabe277d4f6c2b1bf6b6e3be8
Vitaliy Senenko <senenkovitalik@gmail.com>
2016-05-27 21:51:48 +0300

    refactot: Player
    Player:
    - Setting the Player initial location
    
    Player.prototype:
    - The update method
    - The render method

--- js/app.js
+++ js/app.js
@@ -27,7 +27,18 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(loc) {
+    this.sprite = 'image/char-boy.png';
+    this.loc = loc;
+}
 
+Player.prototype.update = function(dt) {
+    this.loc += this.loc*dt;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
